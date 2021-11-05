9ccb5a539c4e4c47b8594c9877e19249f46ce424
Jimmy <jimmy@pop-os.localdomain>
2021-11-05 13:21:05 -0600

    took out a silly print statement

--- morphct/system.py
+++ morphct/system.py
@@ -116,7 +116,6 @@ class System():  # pragma: no cover
         """
         start = len(self.chromophores)
         for i, ind in enumerate(indices):
-            print("hello")
             self._chromophores.append(
                 Chromophore(
                     i+start,
