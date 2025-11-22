35e5ded32178efb855b9a7ba62a8d6cb08bff7b8
chrisjonesBSU <chrisjones4@protonmail.com>
2025-11-22 11:52:48 +0000

    Update environment.yml, and replace setup.py with pyproject.toml

--- morphct/__init__.py
+++ morphct/__init__.py
@@ -1,17 +1,23 @@
 from morphct.chromophores import Chromophore
 from morphct.mobility_kmc import Carrier
-from morphct.__version__ import __version__
-from morphct import (chromophores, execute_qcc, helper_functions, kmc_analyze,
-                mobility_kmc, transfer_integrals)
+from morphct import (
+    chromophores,
+    execute_qcc,
+    helper_functions,
+    kmc_analyze,
+    mobility_kmc,
+    transfer_integrals
+)
 
 __all__ = [
-        "__version__",
-        "Chromophore",
-        "Carrier",
-        "chromophores",
-        "execute_qcc",
-        "helper_functions",
-        "kmc_analyze",
-        "mobility_kmc",
-        "transfer_integrals",
-        ]
+    "Chromophore",
+    "Carrier",
+    "chromophores",
+    "execute_qcc",
+    "helper_functions",
+    "kmc_analyze",
+    "mobility_kmc",
+    "transfer_integrals",
+]
+
+__version__ = "0.5.1"
